d78d65bd59d150412a33a68d5b3632772f947de7
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-11 13:33:21 +0100

    Initial Commit

---
+++ Atelier6-State/Atelier6_StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atelier6_StateApp.swift
+//  Atelier6-State
+//
+//  Created by CHEBIHI FAYCAL on 11/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct Atelier6_StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
